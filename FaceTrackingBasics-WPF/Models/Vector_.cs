6ec2137513155c13b751648085cdf4602bb5beaa
bensearle <bensearle93@hotmail.co.uk>
2015-01-28 21:43:28 +0000

    create Vector_ type - takes X, XY, XYZ vectors, .NET standard has Vector and Vector3D as separate things

---
+++ FaceTrackingBasics-WPF/Models/Vector_.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FaceTrackingBasics.Models
+{
+    public class Vector_
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Vector_()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+
+        public Vector_(double x)
+        {
+            X = x;
+            Y = 0;
+            Z = 0;
+        }
+
+        public Vector_(double x, double y)
+        {
+            X = x;
+            Y = y;
+            Z = 0;
+        }
+
+        public Vector_(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
