ed8b03834c7d9bd41a2d251bd505a2a6d4da58f1
bensearle <bensearle93@hotmail.co.uk>
2015-03-06 13:18:05 +0000

    Tried to have NamePointPair as struct, but changed back to class

--- FaceTrackingBasics-WPF/Models/NamePointPair.cs
+++ FaceTrackingBasics-WPF/Models/NamePointPair.cs
@@ -7,12 +7,29 @@ using System.Threading.Tasks;
 
 namespace FaceTrackingBasics.Models
 {
+/*    [Serializable]
+    public struct NamePointPair<String, Unit3D>
+    {
+        public abstract NamePointPair(String name, Unit3D point);
+
+        public String Name { get; }
+
+        public Unit3D Point { get; }
+
+        public override string ToString()
+        {
+            string ss = string.Format("{0} : {1}",
+                Name, Point.ToString());
+            return ss;
+        }
+    }
+*/
     class NamePointPair
     {
         public String Name { get; set; }
-        public Point Point { get; set; }
+        public Unit3D Point { get; set; }
 
-        public NamePointPair(String s, Point p)
+        public NamePointPair(String s, Unit3D p)
         {
             Name = s;
             Point = p;
@@ -21,8 +38,8 @@ namespace FaceTrackingBasics.Models
 
         public override string ToString()
         {
-            String ss = String.Format("{0} : x = {1}, y = {2}",
-                Name, Point.X, Point.Y);
+            String ss = String.Format("{0} : x = {1}, y = {2}, z = {3}",
+                Name, Point.X, Point.Y, Point.Z);
             return ss;
         }
 
