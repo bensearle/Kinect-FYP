d181dc194a5fba461bffd7bb5ca3b5ac2eda76f1
bensearle <bensearle93@hotmail.co.uk>
2015-03-06 12:15:35 +0000

    Add NamePointPair as datatype for tracked joints

---
+++ FaceTrackingBasics-WPF/Models/NamePointPair.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Kinect.Toolkit.FaceTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceTrackingBasics.Models
+{
+    class NamePointPair
+    {
+        public String Name { get; set; }
+        public Point Point { get; set; }
+
+        public NamePointPair(String s, Point p)
+        {
+            Name = s;
+            Point = p;
+        }
+
+
+        public override string ToString()
+        {
+            String ss = String.Format("{0} : x = {1}, y = {2}",
+                Name, Point.X, Point.Y);
+            return ss;
+        }
+
+    }
+}
