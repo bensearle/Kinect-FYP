2873ceadf6abe75c63d608f2b6f4b6b578f21088
bensearle <bensearle93@hotmail.co.uk>
2015-03-06 13:17:17 +0000

    Add class to track skeletons

---
+++ FaceTrackingBasics-WPF/SkeletalTracking.cs
@@ -0,0 +1,16 @@
+﻿using FaceTrackingBasics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace FaceTrackingBasics
+{
+    class SkeletalTracking
+    {
+        
+        
+
+    }
+}
