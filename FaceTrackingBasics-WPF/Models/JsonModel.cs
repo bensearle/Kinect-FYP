b2baabb7d313b9dd88e8d8fedcdbbc2d11ec6de5
bensearle <bensearle93@hotmail.co.uk>
2015-03-08 16:26:22 +0000

    add JsonModel

---
+++ FaceTrackingBasics-WPF/Models/JsonModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceTrackingBasics.Models
+{
+    class JsonModel
+    {
+        public string JsonString = "";
+
+        public JsonModel(string[] skeletons)
+        {
+            bool firstSkeleton = true;
+
+            JsonString += "{";
+            for (int i = 0; i < skeletons.Length; i++)
+            {
+                if (skeletons[i].Length > 0) // if there is json for that skeleton
+                {
+                    if (!firstSkeleton) // if it is not the first skeleton
+                    {
+                        JsonString += ","; // put a comma before
+                    }
+
+                    JsonString += skeletons[i]; // at the skelton json to the string
+                    firstSkeleton = false;
+                }
+            }
+            JsonString += "}";
+        }
+    }
+}
