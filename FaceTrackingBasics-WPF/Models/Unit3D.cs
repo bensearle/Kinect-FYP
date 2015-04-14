af1754d85a49e218b406204021f5a6079bccb459
bensearle <bensearle93@hotmail.co.uk>
2015-04-14 14:26:06 +0100

    use String.Format + add toJson

--- FaceTrackingBasics-WPF/Models/Unit3D.cs
+++ FaceTrackingBasics-WPF/Models/Unit3D.cs
@@ -35,10 +35,14 @@ namespace FaceTrackingBasics.Models
             Z = z;
         }
 
-
         public override string ToString()
         {
-            return "X: " + X + " Y: " + Y + " Z: " + Z;
+            return String.Format("({0},{1},{2})", X, Y, Z);
+        }
+
+        public string ToJson()
+        {
+            return String.Format("{{\"x\":{0},\"y\":{1},\"z\":{2}}}", X, Y, Z);
         }
     }
 
