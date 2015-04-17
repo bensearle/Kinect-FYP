0532ff3894f0b14dd2f01dd55e33a18b52aa5cc5
bensearle <bensearle93@hotmail.co.uk>
2015-04-17 20:00:19 +0100

    fix toJson()

--- FaceTrackingBasics-WPF/Models/JointPoints.cs
+++ FaceTrackingBasics-WPF/Models/JointPoints.cs
@@ -60,8 +60,6 @@ namespace FaceTrackingBasics.Models
 
         public string ToJson()
         {
-            return String.Format("{{\"x\":{0},\"y\":{1},\"z\":{2}}}", X, Y, Z);
-
             String s = "";
 
             foreach (NamePointPair1 joint in joints)
